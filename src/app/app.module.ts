910384599759ba74858bb869c3e5b80db6e66319
Pedro Neto <pedromneto97@gmail.com>
2018-12-18 11:26:04 -0200

    Modulos e rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
