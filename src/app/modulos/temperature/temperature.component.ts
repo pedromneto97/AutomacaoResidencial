d202a1d795b67531c4cf69529157aa18b90b0f49
pedromneto97 <pedromneto97@gmail.com>
2018-12-21 21:29:20 -0200

    Módulo de temperatura

---
+++ src/app/modulos/temperature/temperature.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {Temperature} from '../../models/temperature';
+import {NgModel} from '@angular/forms';
+import {faTemperatureHigh, faTemperatureLow, faThermometerEmpty} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-temperature',
+  templateUrl: './temperature.component.html',
+  styleUrls: ['./temperature.component.css']
+})
+export class TemperatureComponent implements OnInit {
+
+  public temperature: Temperature;
+  public temp_icon;
+  types = Temperature.types;
+
+  constructor() {
+    this.temperature = new Temperature(true, 0, Temperature.types.find(o => o.name === 'Celsius'));
+    this.temp_icon = faThermometerEmpty;
+
+    this.temp();
+  }
+
+  ngOnInit() {
+  }
+
+  temp() {
+    if (this.temperature.temperature < 20) {
+      this.temp_icon = faTemperatureLow;
+    } else {
+      this.temp_icon = faTemperatureHigh;
+    }
+  }
+
+  tempFilter(value: string) {
+    this.temperature.type = Temperature.types.find(o => o.name === value);
+  }
+}
