2882b6e49877b0bc7ef7dadb5f617e59a469c3d0
Pedro Neto <pedromneto97@gmail.com>
2019-03-01 20:08:49 -0300

    Sensor

--- src/app/models/sensor.ts
+++ src/app/models/sensor.ts
@@ -3,6 +3,10 @@ import {SensorType} from './sensor-type.enum';
 export class Sensor {
   private _type: SensorType;
 
+  constructor(type: SensorType) {
+    this._type = type;
+  }
+
   get type() {
     return this._type;
   }
