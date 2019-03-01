4f55d7daad4a97d7bd184c02a0005479fe11f854
Pedro Neto <pedromneto97@gmail.com>
2019-03-01 14:18:02 -0300

    Room Type Enum

---
+++ src/app/models/room-type.enum.ts
@@ -0,0 +1,12 @@
+export enum RoomType {
+  Kitchen = 'Kitchen',
+  Bedroom = 'Bedroom',
+  Hall = 'Hall',
+  Living_Room = 'Living Room',
+  Dining_Room = 'Dining Room',
+  Office = 'Office',
+  Toilet = 'Toilet',
+  Bathroom = 'Bathroom',
+  Garage = 'Garage',
+  Basement = 'Basement'
+}
