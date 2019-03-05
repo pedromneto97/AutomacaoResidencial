60aa4c4b56cb110995c87b17f30835091de14338
Pedro Neto <pedromneto97@gmail.com>
2019-03-05 15:12:08 -0300

    Residence

--- src/app/models/residence.ts
+++ src/app/models/residence.ts
@@ -1,33 +1,46 @@
-import {ResidenceType} from './residence-type.enum';
 import {Room} from './room';
+import {ResidenceType} from './residence-type';
+import {Address} from './address';
+import {User} from './user';
 
 export class Residence {
-  constructor(residence: string, name: string, type: ResidenceType, rooms: Room[]) {
+  private _residence: string;
+  private _name: string;
+  private _type: ResidenceType;
+  private _rooms: Room[];
+
+  constructor(residence: string, name: string, type: ResidenceType, rooms: Room[], address: Address, users: User[]) {
     this._residence = residence;
     this._name = name;
     this._type = type;
     this._rooms = rooms;
+    this._address = address;
+    this._users = users;
   }
 
-  private _residence: string;
+  private _address: Address;
+
+  get address(): Address {
+    return this._address;
+  }
+
+  private _users: User[];
+
+  get users(): User[] {
+    return this._users;
+  }
 
   get residence(): string {
     return this._residence;
   }
 
-  private _name: string;
-
   get name(): string {
     return this._name;
   }
 
-  private _type: ResidenceType;
-
   get type(): ResidenceType {
     return this._type;
   }
-
-  private _rooms: Room[];
 
   get rooms(): Room[] {
     return this._rooms;
