c6f249ac8f94ef2643be8f9a9ca697c366cfcdb5
Pedro Neto <pedromneto97@gmail.com>
2019-03-05 15:32:18 -0300

    Sensor room

--- src/app/models/sensor.ts
+++ src/app/models/sensor.ts
@@ -1,13 +1,40 @@
-import {SensorType} from './sensor-type.enum';
+import {SensorType} from './SensorType';
 
 export class Sensor {
-  private _type: SensorType;
-
-  constructor(type: SensorType) {
+  constructor(id: string, type: SensorType, patter: number, name: string) {
+    this._id = id;
     this._type = type;
+    this._patter = patter;
+    this._name = name;
   }
 
-  get type() {
+  private _type: SensorType;
+
+  private _id: string;
+
+  get id(): string {
+    return this._id;
+  }
+
+  private _type_id: string;
+
+  get type_id(): string {
+    return this._type_id;
+  }
+
+  private _patter: number;
+
+  get patter(): number {
+    return this._patter;
+  }
+
+  private _name: string;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get type(): SensorType {
     return this._type;
   }
 }
