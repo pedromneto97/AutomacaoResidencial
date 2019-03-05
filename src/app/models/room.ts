4148870f49c0d0ec50c99372609705041d67745e
Pedro Neto <pedromneto97@gmail.com>
2019-03-05 15:32:35 -0300

    Icon

--- src/app/models/room.ts
+++ src/app/models/room.ts
@@ -1,15 +1,21 @@
-import {Sensor} from './sensor';
+import {SensorType} from './SensorType';
 import {RoomType} from './room-type';
 
 export class Room {
   private _name: string;
   private _type: RoomType;
-  private _sensors: Sensor[];
 
-  constructor(name: string, type: RoomType, sensors: Sensor[]) {
+  constructor(name: string, type: RoomType, sensors: SensorType[], icon: string) {
     this._name = name;
     this._type = type;
     this._sensors = sensors;
+    this._icon = icon;
+  }
+
+  private _sensors: SensorType[];
+
+  get sensors(): SensorType[] {
+    return this._sensors;
   }
 
   get name(): string {
@@ -20,7 +26,9 @@ export class Room {
     return this._type;
   }
 
-  get sensors(): Sensor[] {
-    return this._sensors;
+  private _icon: string;
+
+  get icon(): string {
+    return this._icon;
   }
 }
