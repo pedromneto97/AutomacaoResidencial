14054ec483d6f7976a6c7762351ba0a9b451b2b1
Pedro Neto <pedromneto97@gmail.com>
2019-03-07 18:12:20 -0300

    Routing to children modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,60 +1,25 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {fas} from '@fortawesome/free-solid-svg-icons';
-import {BedroomComponent} from './bedroom/bedroom.component';
-import {TemperatureComponent} from './modulos/temperature/temperature.component';
-import {LuminosidadeComponent} from './modulos/luminosidade/luminosidade.component';
 import {far} from '@fortawesome/free-regular-svg-icons';
-import {HumidityComponent} from './modulos/humidity/humidity.component';
-import {PresencaComponent} from './modulos/presenca/presenca.component';
-import {DoorComponent} from './modulos/door/door.component';
 import {CrossbarService} from './services/crossbar.service';
 import {TooltipModule} from 'ngx-bootstrap';
-import {RoomComponent} from './room/room.component';
-
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'room',
-    component: RoomComponent
-  },
-  {
-    path: 'bedroom',
-    component: BedroomComponent
-  },
-  {path: '**', redirectTo: ''}
-];
+import {LoginComponent} from './login/login.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    NavbarComponent,
-    HomeComponent,
-    BedroomComponent,
-    TemperatureComponent,
-    LuminosidadeComponent,
-    HumidityComponent,
-    PresencaComponent,
-    DoorComponent,
-    RoomComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
     FontAwesomeModule,
     TooltipModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [CrossbarService],
   bootstrap: [AppComponent]
